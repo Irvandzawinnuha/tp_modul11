ca50d698076d71819af82e37b58d1b81d280c359
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-05-06 01:13:31 +0700

    Add project files.

---
+++ tp_modul11/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibraryAlgebra;
+internal class Program
+{
+    public static object LibraryAlgebra { get; private set; }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Mencari akar-akar....");
+        double[] equation = { 5, -2, -16 };
+        double[] roots = LibraryAlgebra.RootsOf(equation);
+        Console.WriteLine($"Akar persamaan : {roots[0]} dan {roots[1]}");
+
+        double[] anotherRoot = new double[2] { 5, 2, 16 };
+        double[] rootsResult = LibraryAlgebra.RootsSquared(anotherRoot);
+        Console.WriteLine($"Kuadrat persamaan: ({rootsResult[0]}x^2 + {rootsResult[1]}x + {rootsResult[2]})");
+    }
+}
---
+++ tp_modul11/library Algebra.cs
@@ -0,0 +1,33 @@
+﻿namespace tp_modul11
+{
+    public static double[] RootsOf(double[] equ)
+    {
+        double a = equ[0];
+        double b = equ[1];
+        double c = equ[2];
+
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0)
+        {
+            throw new Exception("Equation results in complex numbers");
+        }
+        double[] roots = new double[2];
+        roots[0] = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        roots[1] = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+        return roots;
+
+    }
+    Public static double[] RootsSquared(double[] equ)
+    {
+        double x = equ[0];
+        double y = equ[1];
+
+        double[] rootsResult = new double[3];
+        rootsResult[0] = x * x;
+        rootsResult[1] = 2 * x * y;
+        rootsResult[2] = y * y;
+
+        return rootsResult;
+    }
+}
